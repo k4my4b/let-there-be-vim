10354ae63e918b5e517e4d923a7acf48b37b41c1
Kamyab Sherafat <k4my4b@gmail.com>
2023-05-18 11:02:21 +0100

    added some comments 'cuz why not?

--- init.lua
+++ init.lua
@@ -1,13 +1,20 @@
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 
+-- Install lazy.nvim if it's not already installed 
 if not vim.loop.fs_stat(lazypath) then
 	if vim.fn.executable('curl') == 0 then		
-		vim.notify_once("Couldn't find curl, is it installed on your system?", vim.log.levels.ERROR)
+		vim.notify_once(
+			"Couldn't find curl, is it installed on your system?", 
+			vim.log.levels.ERROR
+		)
 		return 1
 	end
 
 	if vim.fn.executable('tar') == 0 then
-		vim.notify_once("Couldn't find tar, is it installed on your system?", vim.log.levels.ERROR)
+		vim.notify_once(
+			"Couldn't find tar, is it installed on your system?",
+			vim.log.levels.ERROR
+		)
 		return 1
 	end
 
@@ -17,9 +24,14 @@ if not vim.loop.fs_stat(lazypath) then
 
 	print("Downloading lazy.nvim from upstream")
 
+	-- TODO handle results/failure
 	local result = vim.fn.system(curlCmd .. " | " .. tarCmd)
 end
 
 vim.opt.rtp:prepend(lazypath)
 
+-- All plugins under ./lua/plugins/* get picked up by this.
+-- 'plugins' isn't a special or reserved name it simply refers
+-- to the 'plugins' directory under ./lua, it could be named 
+-- anything you like. 
 require('lazy').setup('plugins')
