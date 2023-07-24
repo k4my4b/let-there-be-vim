fb70b9d9b6baa204b935d2303af6350d45916d60
Kamyab Sherafat <k4my4b@gmail.com>
2023-07-24 13:54:15 +0100

    replace nordic with onedark theme

---
+++ lua/plugins/onedark.lua
@@ -0,0 +1,11 @@
+return {
+	'navarasu/onedark.nvim',
+	priority = 1000,
+	config = function()
+		local onedark = require('onedark')
+		onedark.setup({
+			style = 'darker'
+		})
+		onedark.load()
+	end
+}
