4bd6457ae7e6250057acf196168ff6729c0f879b
Kamyab Sherafat <k4my4b@gmail.com>
2023-07-25 18:07:07 +0100

    removed "diagnostic keymaps"
    removed for now till a later date, I will deal with it then

--- lua/defaults/override.lua
+++ lua/defaults/override.lua
@@ -37,9 +37,3 @@ vim.o.completeopt = 'menuone,noselect'
 
 -- Force colour mode
 vim.o.termguicolors = true
-
--- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
